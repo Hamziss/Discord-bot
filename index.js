c61ed4ba86b9344ef98e2a1da994b409fe5cfe55
Hamza <hamzachebbah9999@gmail.com>
2022-02-02 19:19:12 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { Client, Intents } = require('discord.js');
+const creds = require('./secret.json');
+
 var prefix = "!"
+
 const TOKEN = "OTM3NjY0MzYwOTU0ODU5NTcx.YffB_A.1HuISO44eIht5p6_ehR7vGAeVwE"
 
 const client = new Client({
@@ -13,55 +17,77 @@ const client = new Client({
 
 client.on("ready", async() => {
     console.log(`Logged in as ${client.user.tag}`)
+
 })
 
+async function accesSpreadsheet() {
+    const doc = new GoogleSpreadsheet('1jvJ_d-Vdh00OGA9KMlpRikrZtnx2r4lu_ey4pvFdLs8');
+    await doc.useServiceAccountAuth(creds);
+    //loading data from google sheet doc
+    await doc.loadInfo();
+    const sheet = await doc.sheetsByIndex[0];
 
-client.on('messageCreate', message => {
+
+    var Fullarray = [];
+    var idarray = [];
     var PresentArray = [];
-    var Fullarray = ["Hamzes#7678", "Safiro#0749", "Swissou#3825", "JupiterRoue45#3205",
-        "Pa Pou#5397", "ryad.derr213#0073", "Bybus#0806"
-    ]
     var idPresentArray = [];
-    var idarray = ["444594022644711424", "530849194961797140"]
-    if (message.content === '!rachid cmd') {
-        message.channel.send(`**!rachid present**: gives you the list of members that are with you\n**!rachid absent**: gives you list of members that aren't here\n**!rachid dm**: send dms to absent people`);
-    }
-    if (message.author.bot) {
-        return;
-    }
-    if (message.content.includes('!rachid')) {
-
-        if (!message.member.voice.channel) {
-            message.channel.send(`Your aren't in a channel, You have to be in channel to see who else is here.`);
-        } else {
-
-            message.member.voice.channel.members.each(member => {
-                PresentArray.push(`${member.user.tag}`)
-            });
-            message.member.voice.channel.members.each(member => {
-                idPresentArray.push(`${member.user.id}`)
-            });
-            var absent = Fullarray.filter(x => !PresentArray.includes(x));
-            var idabsent = idarray.filter(x => !idPresentArray.includes(x));
-
-            if (message.content === '!rachid present') { message.channel.send(`📝**PRESENT MEMBERS**📝\n${PresentArray.join("\n") || "No members found"}`); }
-            if (message.content === '!rachid absent') { message.channel.send(`📝**ABSENT MEMBERS**📝\n${absent.join("\n") || "No members found"}`); }
 
 
-            if (message.content === '!rachid dm') {
-                idabsent.forEach(element =>
-                    client.users.fetch(element).then((user) => {
-                        user.send('You missed a meeting today hope you doing fine');
-                    })
-                )
+    // get rows data and transform it into arrays to make it more easier to use
+    const rows = await sheet.getRows();
 
-                if (idabsent.length === 0) { message.channel.send("all members are here") } else { message.channel.send(`✅ Dms sent ✅`); }
+    rows.forEach(row => {
+        Fullarray.push(row.discordTag);
+        idarray.push(row.userID);
+    });
+
+
+    //here i started working on the bot
+    client.on('messageCreate', message => {
+
+        //cmd to resume all commands
+        if (message.content === '!rachid cmd') {
+            message.channel.send(`**!rachid present**: gives you the list of members that are with you\n**!rachid absent**: gives you list of members that aren't here\n**!rachid dm**: send dms to absent people`);
+        }
+
+        if (message.content.includes('!rachid')) {
+
+            if (!message.member.voice.channel) {
+                message.channel.send(`Your aren't in a channel, You have to be in channel execute this 😄.`);
+            } else {
+
+                message.member.voice.channel.members.each(member => {
+                    PresentArray.push(`${member.user.tag}`)
+                });
+                message.member.voice.channel.members.each(member => {
+                    idPresentArray.push(`${member.user.id}`)
+                });
+                var absent = Fullarray.filter(x => !PresentArray.includes(x));
+                var idabsent = idarray.filter(x => !idPresentArray.includes(x));
+
+                if (message.content === '!rachid present') { message.channel.send(`📝**PRESENT MEMBERS**📝\n${PresentArray.join("\n") || "No members found"}`); }
+                if (message.content === '!rachid absent') { message.channel.send(`📝**ABSENT MEMBERS**📝\n${absent.join("\n") || "No members found"}`); }
+
+                //cmd to send dms
+                if (message.content === '!rachid dm') {
+                    idabsent.forEach(element =>
+                        client.users.fetch(element).then((user) => {
+                            user.send('💖 You missed a meeting today hope you doing fine 💖');
+                        })
+                    )
+
+                    if (idabsent.length === 0) { message.channel.send("all members are here") } else { message.channel.send(`✅ Dms sent ✅`); }
+                }
             }
         }
-    }
 
-});
+    });
 
+    console.log(idarray);
+
+
+}
 
 
 client.login(TOKEN)
