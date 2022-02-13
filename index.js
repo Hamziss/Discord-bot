36ab88a747e3b8649ef88bbfe3be4580c4d29c7f
Hamza <hamzachebbah9999@gmail.com>
2022-02-13 09:42:11 +0100

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on("ready", async() => {
 })
 
 async function accesSpreadsheet() {
-    const doc = new GoogleSpreadsheet('1jvJ_d-Vdh00OGA9KMlpRikrZtnx2r4lu_ey4pvFdLs8');
+    const doc = new GoogleSpreadsheet(process.env.sheet_id);
     await doc.useServiceAccountAuth(({
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
         private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
